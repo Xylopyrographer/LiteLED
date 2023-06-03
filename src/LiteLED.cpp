56f7721b023ca70047fe8d95cd813d96e3f8c808
Xylopyrographer <xylopyrographer@gmail.com>
2023-06-03 16:21:36 -0600

    Dev branch for LiteLED v1.0.0 (#1)
    Push from local dev folder

--- src/LiteLED.cpp
+++ src/LiteLED.cpp
@@ -1,194 +1,97 @@
-#include "hal/rmt_types.h"
 /*
- * @file LiteLED.cpp
- *
  * RMT-based ESP-IDF driver for WS2812B/SK6812/APA106/SM16703 LED strips
- *  - Copyright (c) 2020 Ruslan V. Uss <unclerus@gmail.com>
- *  - Copyright (c) 2023 Xylopyrographer <xylopyrographer@gmail.com>
  *  - MIT Licensed as described in the file LICENSE
  */
+
+#include <hal/rmt_types.h>
 #include <cstddef>
-#include "hal/gpio_types.h"
+#include <hal/gpio_types.h>
 #include <Arduino.h>
 #include "LiteLED.h"
 #include "llrmt.h"
 
-static const char *TAG = "LiteLED";
-
-// Constructor1
-LiteLED::LiteLED( led_strip_type_t led_type, bool rgbw ) {
-    // populate the LED strip type structure
-    theStrip.type = led_type;
-    theStrip.is_rgbw = rgbw;
-    theStrip.channel = RMT_CHANNEL_0;
-    #ifdef LED_STRIP_BRIGHTNESS
-        theStrip.brightness = 255;     // < Brightness 0..255, call ::show() after change.                                   
-    #endif
-    theStrip.buf = NULL;
-}
-
-// Constructor2
+// constructor
 LiteLED::LiteLED( led_strip_type_t led_type, bool rgbw, rmt_channel_t channel) {
     // populate the LED strip type structure
     theStrip.type = led_type;
     theStrip.is_rgbw = rgbw;
     theStrip.channel = channel;
-    #ifdef LED_STRIP_BRIGHTNESS
-        theStrip.brightness = 255;     // < Brightness 0..255, call ::show() after change.                                   
-    #endif
+    theStrip.brightness = 255;
     theStrip.buf = NULL;
 }
 
-void LiteLED::led_strip_install() {
-    float ratio = (float)APB_CLK_FREQ / LED_STRIP_RMT_CLK_DIV / 1e09f;
-
-    for (size_t i = 0; i < LED_STRIP_TYPE_MAX; i++) {
-        // 0 bit
-        rmt_items[i].bit0.duration0 = (uint32_t)(ratio * led_params[i].t0h);
-        rmt_items[i].bit0.level0 = 1;
-        rmt_items[i].bit0.duration1 = (uint32_t)(ratio * led_params[i].t0l);
-        rmt_items[i].bit0.level1 = 0;
-        // 1 bit
-        rmt_items[i].bit1.duration0 = (uint32_t)(ratio * led_params[i].t1h);
-        rmt_items[i].bit1.level0 = 1;
-        rmt_items[i].bit1.duration1 = (uint32_t)(ratio * led_params[i].t1l);
-        rmt_items[i].bit1.level1 = 0;
-    }
-    return;
-}
-
-esp_err_t LiteLED::led_strip_init(led_strip_t *strip) {  
-    CHECK_ARG(strip && strip->length > 0 && strip->type < LED_STRIP_TYPE_MAX);
-    strip->buf = (uint8_t*) calloc(strip->length, COLOR_SIZE(strip) );
-    // strip.buf = (uint8_t*) calloc(strip->length, COLOR_SIZE(strip) );
-    if (!strip->buf) {
-        ESP_LOGE(TAG, "Cannot init strip - insufficient memory.");
-        return ESP_ERR_NO_MEM;
-    }
-
-    rmt_config_t config = RMT_DEFAULT_CONFIG_TX(strip->gpio, strip->channel);
-    // rmt_config_t config = RMT_DEFAULT_CONFIG_TX(strip.gpio, strip.channel);
-    config.clk_div = LED_STRIP_RMT_CLK_DIV;
-
-    CHECK(rmt_config(&config));
-    CHECK(rmt_driver_install(config.channel, 0, 0));
-
-    CHECK(rmt_translator_init(config.channel, led_params[strip->type].adapter));
-    #ifdef LED_STRIP_BRIGHTNESS
-        // No support for translator context prior to ESP-IDF 4.3
-        CHECK(rmt_translator_set_context(config.channel, strip));
-    #endif
-
-    return ESP_OK;
-}
-
-esp_err_t LiteLED::begin( uint8_t data_pin, size_t length ) {
+esp_err_t LiteLED::begin( uint8_t data_pin, size_t length, bool auto_w ) {
     // add the data pin & length to the structure
     theStrip.length = length;
     theStrip.gpio = (gpio_num_t)data_pin;
-    LiteLED::led_strip_install();
-    return LiteLED::led_strip_init(&theStrip);
-}
-
-// esp_err_t LiteLED::led_strip_free(led_strip_t *strip) {
-//     CHECK_ARG(strip && strip->buf);
-//     free(strip->buf);
-//     CHECK(rmt_driver_uninstall(strip->channel));
-//     return ESP_OK;
-// }
-
-// esp_err_t LiteLED::free() {
-//      return led_strip_free( &theStrip );
-// }
-
-esp_err_t LiteLED::led_strip_flush(led_strip_t *strip) {
-    CHECK_ARG(strip && strip->buf);
-    CHECK(rmt_wait_tx_done(strip->channel, pdMS_TO_TICKS(LED_STRIP_FLUSH_TIMEOUT)));
-    ets_delay_us(LED_STRIP_PAUSE_LENGTH);
-    return rmt_write_sample(strip->channel, strip->buf,
-                            strip->length * COLOR_SIZE(strip), false);
-}
-
-bool LiteLED::led_strip_busy(led_strip_t *strip) {
-    if (!strip) return false;
-    return rmt_wait_tx_done(strip->channel, 0) == ESP_ERR_TIMEOUT;
-}
-
-esp_err_t LiteLED::led_strip_wait(led_strip_t *strip, TickType_t timeout) {
-    CHECK_ARG(strip);
-    return rmt_wait_tx_done( strip->channel, pdMS_TO_TICKS( timeout ) );
+    theStrip.auto_w = auto_w;
+    led_strip_install();
+    return led_strip_init(&theStrip);
 }
 
 esp_err_t LiteLED::show() {
-    return LiteLED::led_strip_flush( &theStrip );
+    return led_strip_flush( &theStrip );
 }
 
-esp_err_t LiteLED::led_strip_set_pixel(led_strip_t *strip, size_t num, rgb_t color) {
-    CHECK_ARG(strip && strip->buf && num <= strip->length);
-    size_t idx = num * COLOR_SIZE(strip);
-    switch (led_params[strip->type].order) {
-        case ORDER_GRB:
-            strip->buf[idx] = color.g;
-            strip->buf[idx + 1] = color.r;
-            strip->buf[idx + 2] = color.b;
-            if (strip->is_rgbw)
-                strip->buf[idx + 3] = rgb_luma(color);
-            break;
-        case ORDER_RGB:
-            strip->buf[idx] = color.r;
-            strip->buf[idx + 1] = color.g;
-            strip->buf[idx + 2] = color.b;
-            if (strip->is_rgbw)
-                strip->buf[idx + 3] = rgb_luma(color);
-            break;
-    }
-    return ESP_OK;
-}
-
-esp_err_t LiteLED::setPixel( size_t num, rgb_t color ) {
-    return LiteLED::led_strip_set_pixel( &theStrip, num, color );
-}
-
-esp_err_t LiteLED::led_strip_set_pixels(led_strip_t *strip, size_t start, size_t len, rgb_t *data)
-{
-    CHECK_ARG(strip && strip->buf && len && start + len <= strip->length);
-    for (size_t i = 0; i < len; i++)
-        CHECK(LiteLED::led_strip_set_pixel(strip, i + start, data[i]));
-    return ESP_OK;
-}
-
-esp_err_t LiteLED::setPixels( size_t start, size_t len, rgb_t *data) {
-    return LiteLED::led_strip_set_pixels( &theStrip, (size_t)0, (size_t)theStrip.length, data );
-}
-
-esp_err_t LiteLED::led_strip_fill(led_strip_t *strip, size_t start, size_t len, rgb_t color)
-{
-    CHECK_ARG(strip && strip->buf && len && start + len <= strip->length);
-
-    for (size_t i = start; i < start + len; i++)
-        CHECK(LiteLED::led_strip_set_pixel(strip, i, color));
-    return ESP_OK;
-}
-
-
-esp_err_t LiteLED::fill( rgb_t color ) {
-    return LiteLED::led_strip_fill( &theStrip, (size_t)0, (size_t)theStrip.length, color );
-}
-
-esp_err_t LiteLED::clear() {
-    esp_err_t _res = LiteLED::led_strip_fill( &theStrip, (size_t)0, (size_t)theStrip.length, rgb_from_code( 0x000000 ) );
-    if (_res != ESP_OK )
+esp_err_t LiteLED::setPixel( size_t num, rgb_t color, bool show ) {
+    esp_err_t _res = led_strip_set_pixel( &theStrip, num, color );
+    if ( _res != ESP_OK )
         return _res;
-    return LiteLED::led_strip_flush(&theStrip);
+    if ( show )
+        _res = led_strip_flush( &theStrip );
+    return _res;
 }
 
-esp_err_t LiteLED::brightness( uint8_t bright ) {
-    #ifdef LED_STRIP_BRIGHTNESS
-        theStrip.brightness = bright;
-        return LiteLED::led_strip_flush(&theStrip);
-    #else
-        return ESP_ERR_INVALID_ARG;
-    #endif
+esp_err_t LiteLED::setPixel( size_t num, crgb_t color, bool show ) {
+    esp_err_t _res = led_strip_set_pixel( &theStrip, num, rgb_from_code( color ) );
+    if ( _res != ESP_OK )
+        return _res;
+    if ( show )
+        _res = led_strip_flush( &theStrip );
+    return _res;
+}
+
+esp_err_t LiteLED::setPixels( size_t start, size_t len, rgb_t *data, bool show ) {
+    esp_err_t _res = led_strip_set_pixels( &theStrip, (size_t)0, (size_t)theStrip.length, data );
+    if ( _res != ESP_OK )
+        return _res;
+    if ( show )
+        _res = led_strip_flush( &theStrip );
+    return _res;
+}
+
+esp_err_t LiteLED::setPixels( size_t start, size_t len, crgb_t* data, bool show ) {
+    return led_strip_set_pixels_c( &theStrip, start, len, data );
+}
+
+esp_err_t LiteLED::fill( rgb_t color, bool show ) {
+    esp_err_t _res = led_strip_fill( &theStrip, (size_t)0, (size_t)theStrip.length, color );
+    if ( _res != ESP_OK )
+        return _res;
+    if ( show )
+        _res = led_strip_flush( &theStrip );
+    return _res;
+}
+
+esp_err_t LiteLED::fill( crgb_t color, bool show ) {
+    return LiteLED::fill( rgb_from_code( color ), show );
+}
+
+esp_err_t LiteLED::clear( bool show ) {
+    esp_err_t _res = led_strip_fill( &theStrip, (size_t)0, (size_t)theStrip.length, rgb_from_code( 0x000000 ) );
+    if ( _res != ESP_OK )
+        return _res;
+    if ( show )
+        _res = led_strip_flush( &theStrip );
+    return _res;
+}
+
+esp_err_t LiteLED::brightness( uint8_t bright, bool show ) {
+    theStrip.brightness = bright;
+    if (show)
+        return led_strip_flush( &theStrip );
+    else
+        return ESP_OK;
 }
 
 
--- src/LiteLED.h
+++ src/LiteLED.h
@@ -1,35 +1,27 @@
 /*
  * RMT-based ESP-IDF driver for WS2812B/SK6812/APA106/SM16703 LED strips
- *
- *  Copyright (c) 2020 Ruslan V. Uss <unclerus@gmail.com>
- *  Copyright (c) 2023 Xylopyrographer <xylopyrographer@gmail.com>
- *  MIT Licensed as described in the file LICENSE
+ * MIT Licensed as described in the file LICENSE
 */
 
-#include "hal/rmt_types.h"
-
 #pragma once
- 
+
 #ifndef __LITELED_H__
 #define __LITELED_H__
 
-// ??? Move the next 3 lines to LiteLED.cpp ???
+#include <hal/rmt_types.h>
 #include <driver/gpio.h>
 #include <esp_err.h>
 #include <driver/rmt.h>
 #include "llrgb.h"
 
-#define LED_STRIP_FLUSH_TIMEOUT  1000
-#define LED_STRIP_PAUSE_LENGTH   50
-#define LED_STRIP_SHOW_TIMEOUT   1000
+#if ESP_IDF_VERSION < ESP_IDF_VERSION_VAL(4, 4, 1) || ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 0, 0)
+    #error "LiteLED requires arduino-esp32 core version to be at least 2.0.3 but less than 3.0.0."
+#endif
 
-// enum class led_strip_type_t {
-//     LED_STRIP_WS2812 = 0,
-//     LED_STRIP_SK6812,
-//     LED_STRIP_APA106,
-//     LED_STRIP_SM16703,
-//     LED_STRIP_TYPE_MAX
-// };
+// following defines are 'magic numbers' from the esp-idf-library led_strip driver
+#define LED_STRIP_FLUSH_TIMEOUT  1000UL
+#define LED_STRIP_PAUSE_LENGTH   50UL
+#define LED_STRIP_SHOW_TIMEOUT   1000UL
 
 enum led_strip_type_t {
     LED_STRIP_WS2812 = 0,
@@ -39,208 +31,76 @@ enum led_strip_type_t {
     LED_STRIP_TYPE_MAX
 };
 
-#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(4, 3, 0)
-    #define LED_STRIP_BRIGHTNESS
-#endif
-
-// #if ESP_IDF_VERSION < ESP_IDF_VERSION_VAL(4, 3, 0)
-//     #error "LiteLED requires ESP-IDF Version 4.3.0 or greater."
-// #endif
-
 // LED strip descriptor
-// Note: .brightness is supported only for ESP-IDF version >= 4.3
-//       and for ESP-IDF version < 5 
 typedef struct {
     led_strip_type_t type;      // < LED type
-    bool is_rgbw;               // < true for RGBW strips
-    #ifdef LED_STRIP_BRIGHTNESS
-        uint8_t brightness;     // < Brightness 0..255, call ::show() after change.                                   
-    #endif
-    size_t length;              // < Number of LEDs in strip
-    gpio_num_t gpio;            // < Data GPIO pin
-    rmt_channel_t channel;      // < RMT channel
-    uint8_t *buf;               // < Xylopyrographer: pointer to the buffer that contains the LED data 
+    bool is_rgbw;               // < set true for RGBW strips
+    bool auto_w;                // < set false to disable auto W channel setting for RBGW strips
+    uint8_t brightness;         // < brightness 0..255, call ::show() after change.                                   
+    size_t length;              // < number of LEDs in strip
+    gpio_num_t gpio;            // < GPIO pin connected to the strip DIN
+    rmt_channel_t channel;      // < ESP32 RMT channel to use
+    uint8_t *buf;               // < pointer to the buffer that holds the LED data 
 } led_strip_t;
-
 
 class LiteLED {
     public:
-        // Constructor1
-        LiteLED( led_strip_type_t led_type, bool rgbw);
+        // @brief Class constructor. Set the LED parameters for the RMT driver
+        // @param led_type Enumerated value for the type of LED's in the strip
+        // @param rgbw Set true if the strip is RGBW type
+        // @param channel Optional. Enumerated RMT channel to use for this strip
+        // @return `ESP_OK` on success
+        LiteLED( led_strip_type_t led_type, bool rgbw, rmt_channel_t channel = RMT_CHANNEL_0 );
 
-        // Constructor2
-        LiteLED( led_strip_type_t led_type, bool rgbw, rmt_channel_t channel);
-    
-        // Destructor??
-        // LiteLED::~LiteLED();
+        // @brief Initialize the strip
+        // @param data_pin GPIO pin connected to the DIN pin of the strip
+        // @param length Number of LED's in the strip
+        // @param auto_w Optional. Only used for RGBW strips. Set false to not use the automatic W channel value set by the library
+        // @return `ESP_OK` on success
+        esp_err_t begin( uint8_t data_pin, size_t length, bool auto_w = 1 );
 
-        // #ifdef __cplusplus
-        //     extern "C" {
-        // #endif
-
-        /*
-         * @brief Install driver and initialize LED strip
-         *
-         * This method must be called after the constructor and before any other LiteLED methods
-        */        
-        esp_err_t begin(uint8_t data_pin, size_t length );
-
-        /*
-         * @brief Wait until RMT peripherals is free to send buffer to LEDs
-         *
-         * @param strip Descriptor of LED strip
-         * @param timeout Timeout in RTOS ticks
-         * @return `ESP_OK` on success
-        */
-        // esp_err_t led_strip_wait(led_strip_t *strip, TickType_t timeout);
+        // @brief Send the LED buffer to the strip
         esp_err_t show();
 
-        /*
-         * @brief Set color of single LED in strip
-         *
-         * This function does not actually change colors of the LEDs.
-         * Call ::led_strip_flush() to send buffer to the LEDs.
-         *
-         * @param strip Descriptor of LED strip
-         * @param num LED number, 0...strip length - 1
-         * @param color RGB color
-         * @return `ESP_OK` on success
-        */
-        // esp_err_t led_strip_set_pixel(led_strip_t *strip, size_t num, rgb_t color);
-        esp_err_t setPixel( size_t num, rgb_t color);
+        // @brief Set color of single LED in strip, optionally send buffer to the strip
+        // @param num Position of the LED in the strip, 0-based
+        // @param color Colour to set the strip to
+        // @param show Optional. Set true to send the buffer to the strip before returning 
+        // @return `ESP_OK` on success 
+        esp_err_t setPixel( size_t num, rgb_t color, bool show = 0);
+        esp_err_t setPixel( size_t num, crgb_t color, bool show = 0);
 
-        /*
-         * @brief Set colors of multiple LEDs
-         *
-         * This function does not actually change colors of the LEDs.
-         * Call ::led_strip_flush() to send buffer to the LEDs.
-         *
-         * @param strip Descriptor of LED strip
-         * @param start First LED index, 0-based
-         * @param len Number of LEDs
-         * @param data Pointer to RGB data
-         * @return `ESP_OK` on success
-        */
-        // esp_err_t led_strip_set_pixels(led_strip_t *strip, size_t start, size_t len, rgb_t *data);
-        esp_err_t setPixels( size_t start, size_t len, rgb_t *data);
+        // @brief Set colors of multiple LEDs, optionally send buffer to the LEDs
+        // @param start First LED index, 0-based
+        // @param len Number of strip LEDs
+        // @param data Pointer to data. Layout must match the colour type.
+        // @param show Optional. Set true to send the buffer to the strip before returning
+        // @return `ESP_OK` on success
+        esp_err_t setPixels( size_t start, size_t len, rgb_t *data, bool show = 0);
+        esp_err_t setPixels( size_t start, size_t len, crgb_t *data, bool show = 0);
 
-        /*
-         * @brief Set multiple LEDs to the one color
-         *
-         * This function does not actually change colors of the LEDs.
-         * Call ::led_strip_flush() to send buffer to the LEDs.
-         *
-         * @param strip Descriptor of LED strip
-         * @param start First LED index, 0-based
-         * @param len Number of LEDs
-         * @param color RGB color
-         * @return `ESP_OK` on success
-        */
-        // esp_err_t led_strip_fill(led_strip_t *strip, size_t start, size_t len, rgb_t color);
-        esp_err_t fill( rgb_t color );
+        // @brief Set the strip to one color, optionally send buffer to the LEDs
+        // @param color rgb_t or crgb_t colour
+        // @param show Optional. Set true to send the buffer to the strip before returning 
+        // @return `ESP_OK` on success
+        esp_err_t fill( rgb_t color, bool show = 0 );
+        esp_err_t fill( crgb_t color , bool show = 0 );
 
-        /*
-         * @brief Set all LEDS to colour 0
-         *
-         * This function does not actually change colors of the LEDs.
-         * Call ::show() to send buffer to the LEDs.
-         *
-         * @param strip Descriptor of LED strip
-         * @return `ESP_OK` on success
-        */
-        esp_err_t clear();
+        // @brief Clear the strip buffer, optionally send buffer to the LEDs
+        // @param show Optional. Set true to send the buffer to the strip before returning
+        // @return `ESP_OK` on success
+        esp_err_t clear( bool show = 0 );
         
-        /*
-         * @brief Set the intensity of the LEDs
-         *
-         * This function does not actually change intensity of the LEDs.
-         * Call ::show() to send buffer to the LEDs.
-         *
-         * @param strip Descriptor of LED strip
-         * @return `ESP_OK` on success
-        */
-        esp_err_t brightness( uint8_t bright );
-
+        // @brief Set the intensity of the LEDs, optionally send buffer to the LEDs
+        // @param bright Brightness value
+        // @param show Optional. Set true to set strip intensity to `bright` before returning
+        // @return `ESP_OK` on success
+        esp_err_t brightness( uint8_t bright, bool show = 0 );
 
     private:
+        led_strip_t theStrip;   // LED strip object for this class
 
-        /*
-         * @brief Setup library
-         *
-         * This method must be called before any other led_strip methods
-         * RJL - called via ::begin()
-        */
-        // void led_strip_install();
-        void led_strip_install();
-
-        /*
-         * @brief Initialize LED strip and allocate buffer memory
-         *
-         * @param strip Descriptor of LED strip
-         * @return `ESP_OK` on success
-         * RJL - called via ::begin()
-        */
-        // esp_err_t led_strip_init(led_strip_t *strip);
-        esp_err_t led_strip_init( led_strip_t *strip );
-
-        /*
-         * @brief Set color of single LED in strip
-         *
-         * This function does not actually change colors of the LEDs.
-         * Call ::led_strip_flush() to send buffer to the LEDs.
-         *
-         * @param strip Descriptor of LED strip
-         * @param num LED number, 0...strip length - 1
-         * @param color RGB color
-         * @return `ESP_OK` on success
-        */
-        esp_err_t led_strip_set_pixel(led_strip_t *strip, size_t num, rgb_t color);
-
-        esp_err_t led_strip_set_pixels(led_strip_t *strip, size_t start, size_t len, rgb_t *data);
-
-        esp_err_t led_strip_fill(led_strip_t *strip, size_t start, size_t len, rgb_t color);
-
-        /*
-         * @brief Deallocate buffer memory and release RMT channel
-         *
-         * @param strip Descriptor of LED strip
-         * @return `ESP_OK` on success
-        */
-        // esp_err_t led_strip_free(led_strip_t *strip);
-        // esp_err_t free(led_strip_t *strip);
-
-        /*
-         * @brief Send strip buffer to LEDs
-         *
-         * @param strip Descriptor of LED strip
-         * @return `ESP_OK` on success
-        */
-        esp_err_t led_strip_flush(led_strip_t *strip);
-
-        esp_err_t led_strip_wait(led_strip_t *strip, TickType_t timeout);
-
-        /*
-         * @brief Check if associated RMT channel is busy
-         *
-         * @param strip Descriptor of LED strip
-         * @return true if RMT peripherals is busy
-        */
-        bool led_strip_busy(led_strip_t *strip);
-
-
-        // #ifdef __cplusplus
-        //     }
-        // #endif
-
-        /**@}*/
-
-        led_strip_t theStrip;
-//         led_strip_type_t _led_type;
-//         bool _rgbw;
-//         uint8_t _data_pin;
-//         size_t _length;
-//         rmt_channel_t _channel;
-        
+    
 };   // class LiteLED
 
 #endif /* __LITELED_H__ */
